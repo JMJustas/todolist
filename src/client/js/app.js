24b6a76d6c959694ad3c97be1997430a66018d8a
JMJustas <justinas.marcinka@gmail.com>
2016-04-03 12:30:32 +0300

    add app

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,1 +1,8 @@
-console.log("CLIENT RUNNING");
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodosApp from './components/TodosApp';
+import EntryService from './services/EntryService';
+
+const app = document.getElementById('app');
+ReactDOM.render(<TodosApp entryService={new EntryService}/>, app);
